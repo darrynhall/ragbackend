8a0ec28c5941f023862d315995c34e773d773714
darrynhall <darrynhall@users.noreply.github.com>
2025-06-24 17:45:53 +0000

    Remove target folder from repository

--- target/classes/db/migration/V1__create_chunk_hash_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE chunk_hash (
-    hash VARCHAR(255) PRIMARY KEY,
-    filename VARCHAR(512),
-    ingested_at TIMESTAMP
-);
--- target/classes/db/migration/V2__create_file_hash_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE file_hash (
-    hash VARCHAR(255) PRIMARY KEY,
-    filename VARCHAR(512),
-    size BIGINT,
-    ingested_at TIMESTAMP
-);
